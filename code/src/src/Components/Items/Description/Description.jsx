d292084c937e3e6dc3d5cfd349d0e104b068c5b5
alex3007 <alex300785@tut.by>
2020-07-23 13:34:28 +0300

    commit 21

--- code/src/src/Components/Items/Description/Description.jsx
+++ code/src/src/Components/Items/Description/Description.jsx
@@ -1,79 +1,33 @@
 import React from 'react';
 import cls from './Description.module.css';
-import Preloader from "../../Preloader/Preloader";
 import Fade from 'react-reveal/Fade';
-import ReactModal from 'react-modal';
+import OrderForm from './OrderForm/OrderForm';
 
-export default class Item extends React.Component {
+const Description = (props) => {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            phone: '',
-            modalIsOpen: false,
-            productName: this.props.itemDescription
-        };
-    }
-
-    onNameChange = (name) => {
-        this.setState({userName: name.target.value});
-    };
-    onPhoneChange = (phone) => {
-        this.setState({userName: phone.target.value});
-    };
-    openModal = () => {
-        this.setState({modalIsOpen: true});
-    };
-    closeModal = () => {
-        this.setState({modalIsOpen: false});
-    };
-
-    render() {
-        let itemDescription = this.state.productName;
-        let modalContainer;
-        if (itemDescription !== 'loading') {
-            itemDescription =
-                <Fade>
-                    <div className={cls.container}>
-                        <h4><b>Название товара: </b>{itemDescription.name}</h4>
-                        <img src={itemDescription.photo}/>
-                        <button className={cls.buyButton} onClick={this.openModal}>Заказать</button>
-                        <p><b>Особенности: </b>{itemDescription.description}</p>
-                    </div>
-                </Fade>
-        }
-        else {
-            itemDescription = <Preloader/>
-        }
-        return (
-            <div>
-                {itemDescription}
-                <div>
-                    <ReactModal
-                        isOpen={this.state.modalIsOpen}
-                        onRequestClose={this.closeModal}
-                        className={cls.modal}
-                        overlayClassName={cls.overlay}>
-                        <div>
-                            <h3>Заказ товара</h3>
-                            <form>
-                                <input type="text" placeholder="Название товара"
-                                       value={this.state.productName.name}/><br/>
-                                <input type="text" placeholder="Введите имя"
-                                       required
-                                       onChange={this.onNameChange}/><br/>
-                                <input type="text" placeholder="Введите номер телефона"
-                                       required
-                                       onChange={this.onPhoneChange}/><br/>
-                                <br/>
-                                <button onClick={this.closeModal}>Подтвердить заказ</button>
-                            </form>
-                        </div>
-                    </ReactModal>
+    let itemDescription = props.itemDescription;
+    let modalContainer;
+    if (itemDescription !== 'loading') {
+        itemDescription =
+            <Fade>
+                <div className={cls.container}>
+                    <h4><b>Название товара: </b>{itemDescription.name}</h4>
+                    <img src={itemDescription.photo}/>
+                    <p><b>Особенности: </b>{itemDescription.description}</p>
+                    <OrderForm productName={itemDescription.name} />
                 </div>
-            </div>
-        )
+            </Fade>
     }
-}
+    else {
+        itemDescription = null
+    }
+    return (
+        <div>
+            {itemDescription}
+        </div>
+    )
+};
+
+export default Description;
+
 
---
+++ code/src/src/Components/Items/Description/OrderForm/OrderForm.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import cls from './OrderForm.module.css';
+import ReactModal from 'react-modal';
+
+export default class OrderForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            phone: '',
+            modalIsOpen: false,
+            productName: this.props.productName
+        };
+    }
+
+    onNameChange = (name) => {
+        this.setState({userName: name.target.value});
+    };
+    onPhoneChange = (phone) => {
+        this.setState({userName: phone.target.value});
+    };
+    openModal = () => {
+        this.setState({modalIsOpen: true});
+    };
+    closeModal = () => {
+        this.setState({modalIsOpen: false});
+    };
+
+    render() {
+        return (
+              <div><button className={cls.confirmButton} onClick={this.openModal}>Подтвердить заказ</button>
+                    <ReactModal
+                        isOpen={this.state.modalIsOpen}
+                        onRequestClose={this.closeModal}
+                        className={cls.modal}
+                        overlayClassName={cls.overlay}>
+                        <div>
+                            <h3>Заказ товара</h3>
+                            <form>
+                                <input type="text" placeholder="Название товара"
+                                       value={this.state.productName}/><br/>
+                                <input type="text" placeholder="Введите имя"
+                                       required
+                                       onChange={this.onNameChange}/><br/>
+                                <input type="text" placeholder="Введите номер телефона"
+                                       required
+                                       onChange={this.onPhoneChange}/><br/>
+                                <br/>
+                                <button onClick={this.closeModal}>Подтвердить заказ</button>
+                            </form>
+                        </div>
+                    </ReactModal>
+                </div>
+                    )
+    }
+}
+
